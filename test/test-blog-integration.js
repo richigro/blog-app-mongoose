c45ef0e6c7eb00b777067d57f64bdfc4258b8b46
Ricardo Guerrero <richi_gro@hotmail.com>
2018-10-15 18:01:14 -0500

    added new fucntions to seed test-db

--- test/test-blog-integration.js
+++ test/test-blog-integration.js
@@ -9,4 +9,39 @@ const expect = chai.expect;
 
 const {BlogPost} = require('./models');
 const {app, runServer, closeServer} = require('../server');
-const {}
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+function seedBlogData() {
+    console.info('Seeding blog data');
+    const seedData = [];
+
+    for(let i =0; i <= 10; i++) {
+        seedData.push(generateBlogData());
+    }
+    return BlogPost.insertMany(seedData);
+}
+
+
+
+function generateRandomTitle() {
+    const title = ['The end', 'The begining', 'The middle', 'The Post'];
+    return title[Math.floor(Math.random() * title.length)];
+}
+
+
+function generateRandomContent() {
+    const content = ['ffFFFA', 'ADfFFGGSE', 'Fgagfw', 'DsAd'];
+    return content[Math.floor(Math.random() * content.length)];
+}
+
+
+function generateBlogData() {
+    return {
+        author: faker.name.firstName(),
+        content: generateRandomContent(),
+        title: generateRandomTitle(),
+        created: Date.now()
+    };
+}
